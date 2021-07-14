a10aabcc575ac658ef37c5bef9c7a1a053eabcde
veronika-hai <58940477+veronika-hai@users.noreply.github.com>
2021-07-15 00:59:16 +0200

    weitere Bilder für die 2 anderen Tokens

--- serial1.py
+++ serial1.py
@@ -14,7 +14,9 @@ import time   # Python Time Bibliothek - hilft beim Umgang mit zeitbezogenem Cod
 from PIL import Image # Python Pillow ist eine Image Bibliothek - hilft beim Laden und Anzeigen von Bildern
 # Pillow Doku: https://pillow.readthedocs.io/en/stable/reference/Image.html
 
-img = Image.open('hamster.jpeg') # läd das Bild 
+hamster = Image.open('hamster.jpeg') # laed das Bild 
+katze = Image.open ('katze.jpeg')
+hund = Image.open ('hund.jpeg')
 
 verbindung = serial.Serial('/dev/ttyACM0', 9600) # serial.Serial gibt den Pfad zum Arduino an und die Datenrate
 verbindung.isOpen()       # oeffnet den seriellen Port
@@ -29,11 +31,16 @@ try:                      # solange das Programm laeuft fuehre die Zeilen 17-19 
       
         if 'c4abe59c' in antwort:  # wenn dieser String in der ID vom Arduino vorkommt
             print('hallo hamster') 
-            img.show()             # zeig das Bild
+            hamster.show()             # zeig das Bild
             
-        else:
-            print('no hamster here')
-        
+        if '5430e09c' in antwort:
+            print('hallo katze') 
+            katze.show()
+
+        if '34ddf39c' in antwort:
+            print('hallo hund') 
+            hund.show()
+                    
 except KeyboardInterrupt: # hat Programm durch Strg/Ctrl+C geschlossen 
     verbindung.close()    # schliesst Port wieder
     
